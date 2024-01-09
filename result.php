6b1ae9c4b8df4cf2ec5a56d2d3220d0af0a6d0e4
abisheikM1 <124699114+abisheikM1@users.noreply.github.com>
2024-01-09 15:56:23 +0530

    Create result.php

---
+++ result.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Result</title>
+</head>
+<body>
+  <h1>Result Page</h1>
+  <p>Hello, <?php echo $_GET['userInput']; ?>!</p>
+</body>
+</html>
